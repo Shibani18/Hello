974e99692a54c5037b8085fe638c03215af47b12
Shibani Mehta <mehtas@opentext.com>
2021-10-12 17:36:42 +0530

    added borwser tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,13 @@ kotlin {
             commonWebpackConfig {
                 cssSupport.enabled = true
             }
+testTask {
+                useKarma {
+//                    useFirefox()
+                    useChrome()
+//                    useSafari()
+                }
+            }
         }
     }
     val hostOs = System.getProperty("os.name")
